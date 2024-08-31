5e92ac912fd783ad4792b2847240c434ff9bae76
RosarioManny <rosario.emm47@gmail.com>
2024-08-30 20:58:59 -0400

    Exercise 7 to 9

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ function isAdult(x) {
   }
 }
 
-console.log("Exercise 2:", isAdult(22));
+console.log("Exercise 2:", isAdult(21));
 
 // Exercise 3
 
-function isCharAVowel(x) {
-  if ("a") {
+function isCharAVowel(letter) {
+  if (("a", "e", "i", "o", "u")) {
     return true;
   } else {
     return false;
@@ -36,17 +36,24 @@ console.log("Exercise 3:", isCharAVowel("a"));
 
 // Exercise 4
 
-function generateEmail(x, y) {
-  return x + y;
+function generateEmail(name, domain) {
+  return `${name}@${domain}`;
 }
-console.log("Exercise 4:", generateEmail("SuperMannyDonuts69", "@gmail.com"));
+console.log("Exercise 4:", generateEmail("SuperMannyDonuts69", "gmail.com"));
 
 // Exercise 5
 
-let person = "Manny";
-let day = "Morning";
+function greetUser(person, timeOfDay) {
+  if (timeOfDay === "morning") {
+    return `Good ${timeOfDay}, ${person}!`;
+  } else if (timeOfDay === "afternoon") {
+    return `Good ${timeOfDay}, ${person}!`;
+  } else if (timeOfDay === "morning") {
+    return `Good ${timeOfDay}, ${person}!`;
+  }
+}
 
-console.log("Exercise 5:", `Good ${day}, ${person}!`);
+console.log("Exercise 5:", greetUser("Sam", "morning"));
 
 // Exercise 6
 
@@ -59,18 +66,46 @@ function maxOfThree(x, y, z) {
     return z;
   }
 }
-console.log("Exercise 6:", maxOfThree(3, 16, 69));
+console.log("Exercise 6:", maxOfThree(5, 10, 8));
 
 // Exercise 7
 
 // console.log(Math.max(1, 69, 420));
 
-function calculateTip(x, y) {
-  let billAmount = x;
-  const tipPercent = y / 100;
+function calculateTip(bill, tip) {
+  let billAmount = bill;
+  const tipPercent = tip / 100;
   return billAmount * tipPercent;
 }
 
 console.log("Exercise 7:", calculateTip(50, 20));
 
 // Exercise 8
+
+function convertTemperature(temp, deg) {
+  if (deg === "C") {
+    return `${(temp * 1.8) + 32} Fahrenheit`;
+  } else if (deg === "F") {
+    return `${(temp - 32) / 1.8} Celsius`;
+  }
+}
+
+console.log('Exercise 8:', convertTemperature(32, "C"));
+
+// Exercise 9
+
+function basicCalculator(x, y, operation) {
+  if (operation === "subtract") {
+    return x - y
+  } else if (operation === "add") {
+    return x + y
+  }
+    else if (operation === "multiply") {
+      return x * y
+    }
+      else if (operation === "divide") {
+        return x / y
+}
+}
+
+console.log('Exercise 9:', basicCalculator(10, 5, "subtract"));
